0771c53ed36e0fceea2635e66da7649e25489548
Diego22182026 <00242126@uca.edu.sv>
2026-09-14 11:21:42 -0600

    feat: add:

---
+++ preparcial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+struct Elementos{
+
+    std:: string codigo, nombre;
+    double longitud, capacidadmax;
+    float cargas[3];
+    int factor, estadosegu;
+
+
+};
+
+int main (){
+
+    return 0;
+}
